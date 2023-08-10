535033d5cc459043f2cdd94151b93c57c18a407b
CortezDeveloper <josecortezsoftware@gmail.com>
2023-08-10 17:58:51 +0200

    Updated App CSS Path Carrouselwomen

--- src/Components/Carrouselwomen.jsx
+++ src/Components/Carrouselwomen.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 // import "./App.css"
-import "../../../carrito.com/src/App.css";
+import "./../App.css";
 
 function Carrousel({ type }) {
   const [products, setProducts] = useState([]);
