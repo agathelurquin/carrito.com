ea916d2f41c7ef92b1139eb5936a835723548904
Agathe Lurquin <agathe.lurquin@essca.eu>
2023-08-11 12:02:42 +0200

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,16 +6,23 @@ import Search from "./Search";
 
 import "./../App.css";
 
-
 function NavBar() {
   return (
     <>
       <nav>
         <div className="links-to-pages">
-          <Link to="/" className="nav-link>Home</Link>
-          <Link to="/all-products" className="nav-link>All Products</Link>
-          <Link to="/cat/women" className="nav-link>Women</Link>
-          <Link to="/cat/men" className="nav-link>Men</Link>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/all-products" className="nav-link">
+            All Products
+          </Link>
+          <Link to="/cat/women" className="nav-link">
+            Women
+          </Link>
+          <Link to="/cat/men" className="nav-link">
+            Men
+          </Link>
           <Search></Search>
         </div>
       </nav>
